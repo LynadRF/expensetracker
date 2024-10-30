24a913de855bb541bf696eec22bfeabdf39d53d0
LynadRF <116899118+LynadRF@users.noreply.github.com>
2024-10-30 18:22:00 +0100

    added table initialization

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 
 import user from "./routes/user";
 import record from "./routes/record";
+import { initTables } from "./db/db";
 
 dotenv.config();
 
@@ -16,15 +17,17 @@ const port: string = process.env.PORT || "3001";
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: clientUrl,
-        credentials: true,
-    })
+  cors({
+    origin: clientUrl,
+    credentials: true,
+  })
 );
 
 app.use("/api/user", user);
 app.use("/api/record", record);
 
+// initTables(); // Run this the first time you start the server to create all necessary SQL tables
+
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -4,9 +4,41 @@ import { open } from "sqlite";
 let db;
 
 export const getDb = async () => {
-    db = await open({
-        filename: "src/db/db.sqlite", // Specify the database file
-        driver: sqlite3.Database,
-    });
-    return db;
+  db = await open({
+    filename: "src/db/db.sqlite", // Specify the database file
+    driver: sqlite3.Database,
+  });
+  return db;
+};
+
+const createUsersTableSql = `CREATE TABLE "users" (
+	"id" INTEGER,
+	"email"	TEXT NOT NULL UNIQUE,
+	"username" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	"created_at" TEXT NOT NULL,
+	PRIMARY KEY("id")
+);`;
+
+const createRecordsTableSql = `CREATE TABLE "records" (
+    "id" INTEGER,
+    "description" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "category" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "created_at" TEXT NOT NULL,
+    PRIMARY KEY("id"),
+    FOREIGN KEY (userId) REFERENCES "users"("id")
+);`;
+
+export const initTables = async () => {
+  const db = await getDb();
+
+  try {
+    db.run(createUsersTableSql);
+    db.run(createRecordsTableSql);
+    console.log("Tables created!");
+  } catch (error) {
+    console.error("Error creating db tables:", error);
+  }
 };
